6f65a54f1481a7f6e68633cef329db40133bd54c
nimkoes <xxxelppa@gmail.com>
2021-10-30 14:59:18 +0900

    로그 추적기 적용 V1 준비

---
+++ src/main/java/hello/advanced/app/v1/OrderControllerV1.java
@@ -0,0 +1,19 @@
+package hello.advanced.app.v1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV1 {
+
+    private final OrderServiceV1 orderService;
+
+    @GetMapping("/v1/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+}
