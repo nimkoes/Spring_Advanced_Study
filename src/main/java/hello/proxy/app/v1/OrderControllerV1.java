8430ddb982ee9cef2d0df856376c9be0bb7259dd
nimkoes <xxxelppa@gmail.com>
2021-11-03 22:28:13 +0900

    프록시 패턴과 데코레이터 패턴 > 예제 프로젝트 v1  - Interface 와 implementation 사용

---
+++ src/main/java/hello/proxy/app/v1/OrderControllerV1.java
@@ -0,0 +1,17 @@
+package hello.proxy.app.v1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping // -> Spring 은 @Controller 또는 @RequestMapping annotation 이 있어야 Spring Controller 로 인식
+@ResponseBody       // -> HTTP message converter 를 사용해서 응답
+public interface OrderControllerV1 {
+
+    @GetMapping("/v1/request")
+    String request(@RequestParam("itemId") String itemId);
+
+    @GetMapping("/v1/no-log")
+    String noLog();
+}
