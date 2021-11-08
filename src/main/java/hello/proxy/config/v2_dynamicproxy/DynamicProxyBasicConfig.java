c931ac03c1f96d7fbd351ddaeed38c598e2d1dc8
nimkoes <xxxelppa@gmail.com>
2021-11-08 22:08:16 +0900

    동적 프록시 기술 > JDK 동적 프록시 적용  - JDK dynamic proxy 는 interface 가 반드시 있어야 가능  - @Controller 에 no-log 요청에 대해 로그를 남기는 이슈 남아있는 상태

---
+++ src/main/java/hello/proxy/config/v2_dynamicproxy/DynamicProxyBasicConfig.java
@@ -0,0 +1,56 @@
+package hello.proxy.config.v2_dynamicproxy;
+
+import hello.proxy.app.v1.*;
+import hello.proxy.config.v2_dynamicproxy.handler.LogTraceBasicHandler;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * JDK dynamic proxy 는 interface 가 반드시 필요하기 때문에
+ * 현재 작성한 예제 애플리케이션 중 *V1 에만 적용할 수 있다.
+ */
+@Configuration
+public class DynamicProxyBasicConfig {
+
+    @Bean
+    public OrderControllerV1 orderControllerV1(LogTrace logTrace) {
+        OrderControllerV1 orderControllerV1 = new OrderControllerV1Impl(orderServiceV1(logTrace));
+
+        return (OrderControllerV1) Proxy.newProxyInstance(
+                OrderControllerV1.class.getClassLoader(),
+                new Class[]{OrderControllerV1.class},
+                /**
+                 * LogTraceBasicHandler 의 첫 번째 매개변수인
+                 * target 에 해당하는 Object 가
+                 * Controller, Service, Repository 모두 다르기 때문에
+                 * new 연산자로 객체를 새로 만들어 줘야 한다.
+                 */
+                new LogTraceBasicHandler(orderControllerV1, logTrace)
+        );
+    }
+
+    @Bean
+    public OrderServiceV1 orderServiceV1(LogTrace logTrace) {
+        OrderServiceV1 orderServiceV1 = new OrderServiceV1Impl(orderRepositoryV1(logTrace));
+
+        return (OrderServiceV1) Proxy.newProxyInstance(
+                OrderServiceV1.class.getClassLoader(),
+                new Class[]{OrderServiceV1.class},
+                new LogTraceBasicHandler(orderServiceV1, logTrace)
+        );
+    }
+
+    @Bean
+    public OrderRepositoryV1 orderRepositoryV1(LogTrace logTrace) {
+        OrderRepositoryV1 orderRepository = new OrderRepositoryV1Impl();
+
+        return (OrderRepositoryV1) Proxy.newProxyInstance(
+                OrderRepositoryV1.class.getClassLoader(),
+                new Class[]{OrderRepositoryV1.class},
+                new LogTraceBasicHandler(orderRepository, logTrace)
+        );
+    }
+}
