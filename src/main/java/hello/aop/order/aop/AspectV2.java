087c6df3aae72687330333d336480b352da85b6e
nimkoes <xxxelppa@gmail.com>
2021-11-17 23:10:36 +0900

    Spring AOP 구현 > 예제 프로젝트 구현 2  - Aspect 에서 pointcut 분리  - 재사용 가능  - pointcut public 선언시 다른 Aspect 에서 사용 가능

---
+++ src/main/java/hello/aop/order/aop/AspectV2.java
@@ -0,0 +1,22 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Slf4j
+@Aspect
+public class AspectV2 {
+
+    // hello.aop.order 패키지와 하위 모든 패키지
+    @Pointcut("execution(* hello.aop.order..*(..))")    // pointcut expression
+    private void allOrder(){}   // pointcut signature _ 반환 타입은 void 여야함, 메소드는 구현하지 않음
+
+    @Around("allOrder()")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[log] {}", joinPoint.getSignature()); // join point 시그니처
+        return joinPoint.proceed();
+    }
+}
