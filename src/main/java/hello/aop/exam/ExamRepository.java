42d42e66d440a5d9876e6d957fb5d470902d7cbc
nimkoes <xxxelppa@gmail.com>
2021-11-20 23:01:22 +0900

    Spring AOP > 실전 예제  - 로그 출력 AOP 구현

--- src/main/java/hello/aop/exam/ExamRepository.java
+++ src/main/java/hello/aop/exam/ExamRepository.java
@@ -1,5 +1,6 @@
 package hello.aop.exam;
 
+import hello.aop.exam.annotation.Trace;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,6 +11,7 @@ public class ExamRepository {
     /**
      * 5 번에 1 번 실패하는 요청
      */
+    @Trace
     public String save(String itemId) {
         if (++seq % 5 == 0) {
             throw new IllegalStateException("예외 발생");
